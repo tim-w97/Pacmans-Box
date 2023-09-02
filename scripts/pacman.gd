eadea4a4f9011f67d1e647d764a982702ef0849f
Tim <timwagner997@gmail.com>
2023-09-03 01:58:40 +0200

    enabled touch from mouse / set splash background to black / added rotation to pacman

--- scripts/pacman.gd
+++ scripts/pacman.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+var _speed = 0.5
+
 @onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_animated_sprite.play()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	rotation -= _speed * delta
+	
+	var user_tapped_screen = Input.is_action_pressed("screen_tap")
+	var user_released_screen = Input.is_action_just_released("screen_tap")
+	
+	_animated_sprite.play()
