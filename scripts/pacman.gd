18ecb06a2026384efcd0189da616d2d25096eff6
Tim <timwagner997@gmail.com>
2023-09-03 13:41:12 +0200

    adjust rotation so pacman follows the circle path

--- scripts/pacman.gd
+++ scripts/pacman.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var _speed = 1
-var _orbit_radius = 120
+var _orbit_radius = 150
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
@@ -22,14 +22,17 @@ func _do_orbit_movement():
 	# r = radius
 	# t = angle in radians
 	# (h,k) = center point
-	# (x,y) = desired point on circular path
+	# (x,y) = desired point on circle path
 	
 	# float x = r*cos(t) + h;
 	# float y = r*sin(t) + k;
 	
+	# adjust rotation so pacman follows the circle path
+	var adjusted_rotation = rotation - deg_to_rad(90)
+	
 	position = Vector2(
-		_orbit_radius * cos(rotation) + center.x,
-		_orbit_radius * sin(rotation) + center.y
+		_orbit_radius * cos(adjusted_rotation) + center.x,
+		_orbit_radius * sin(adjusted_rotation) + center.y
 	)
 
 func _input(event):
