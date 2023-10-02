54875476821d0301e7273df14858097fe4b6ecf4
Tim <timwagner997@gmail.com>
2023-10-02 21:31:19 +0200

    added a share button

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -16,10 +16,12 @@ func _process(delta):
 
 func _on_play_button_pressed():
 	score = 0
+	
 	$BottomLabel.hide()
+	$ShareButton.hide()
+	$PlayButton.hide()
 	
 	start_game.emit()
-	$PlayButton.hide()
 
 	$TopLabel.text = str(score)
 
@@ -32,7 +34,9 @@ func _on_pacman_throw_success(new_rotation_direction):
 func _on_pacman_throw_fail():
 	$TopLabel.text = "Game Over"
 	
-	$BottomLabel.show()
 	$BottomLabel.text = "You got " + str(score) + " points"
+	$BottomLabel.show()
+	
+	$ShareButton.show()
 	
 	$PlayButton.show()
