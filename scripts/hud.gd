64091ae374a4bbab2f208d33e49973552e643eb2
Tim <timwagner997@gmail.com>
2023-10-07 18:50:59 +0200

    changed text of score label

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -45,7 +45,7 @@ func _on_pacman_throw_success(new_rotation_direction):
 func _on_pacman_throw_fail():
 	top_label.text = "Game Over"
 	
-	score_label.text = "You got " + str(score) + " points"
+	score_label.text = "Your score is " + str(score)
 	score_label.show()
 	
 	play_button.show()
